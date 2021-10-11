8df790e0f601cdebeb8b628ba5ee93342922f793
CsharpL <igr0ks@ya.ru>
2021-10-11 18:00:30 +0300

    mvc + ЗАПУСК

--- MyCompany/MyCompany/Startup.cs
+++ MyCompany/MyCompany/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,25 +15,24 @@ namespace MyCompany
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // добовляем поддержку контроллеров и представлений (mvc) 
+            services.AddControllersWithViews()
+                //выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {//порядок подклбчения middleware очень важно
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); // вывод подробной инфы при ошибках ,если в разработке
             }
-
             app.UseRouting();
-
+            //подчключаем поддержку статичных файлов в приложении(css,js  ит.д.)
+            app.UseStaticFiles();
+            //регистрируем нужные маршруты (ендпоинты)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
