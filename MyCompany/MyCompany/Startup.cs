483e5259c73fac378115dd9f08b704669e32bc3c
CsharpL <igr0ks@ya.ru>
2021-10-28 21:05:46 +0300

    domain

--- MyCompany/MyCompany/Startup.cs
+++ MyCompany/MyCompany/Startup.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCompany.Domain;
+using MyCompany.Domain.Entities;
+using MyCompany.Domain.Repositories.Abstract;
+using MyCompany.Domain.Repositories.EntityFramework;
+using MyCompany.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +20,44 @@ using System.Threading.Tasks;
 namespace MyCompany
 {
     public class Startup
-    {
+    { 
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            //подключаем конфиг из appsettings.json
+            Configuration.Bind("Project", new Config());
+
+            //подключаем нужный функционал приложения в качестве сервисов 
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //подключаем контекст БД
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //настраиваем identity систему
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+             {
+                 opts.User.RequireUniqueEmail = true;
+                 opts.Password.RequiredLength = 6;
+                 opts.Password.RequireNonAlphanumeric = false;
+                 opts.Password.RequireLowercase = false;
+                 opts.Password.RequireUppercase = false;
+                 opts.Password.RequireDigit = false;
+
+             }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //настраиваем authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
             // добовляем поддержку контроллеров и представлений (mvc) 
             services.AddControllersWithViews()
                 //выставляем совместимость с asp.net core 3.0
@@ -23,12 +66,16 @@ namespace MyCompany
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {//порядок подклбчения middleware очень важно
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage(); // вывод подробной инфы при ошибках ,если в разработке
-            }
-            app.UseRouting();
+       
             //подчключаем поддержку статичных файлов в приложении(css,js  ит.д.)
             app.UseStaticFiles();
+            app.UseRouting();
+
+            //подключаем аунтификацию и авторизацию
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
             //регистрируем нужные маршруты (ендпоинты)
             app.UseEndpoints(endpoints =>
             {
